14bf82c0f77bccbeaab3345960cbef41bdcd8a4c
tarlepp <tarmo.leppanen@protacon.com>
2017-07-14 01:37:28 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
